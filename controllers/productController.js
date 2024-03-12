e5956828a4bf002f37d63265cf3acc9d56193728
Joshua Biong <joshuabiong7@gmail.com>
2024-03-13 00:21:09 +0800

    adding expressasynhandler package

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,21 @@
 const Product = require('../model/productModel');
+const asynhandler = require('express-async-handler')
 
 
 
 
-const getProducts = async(req, res) => {
+const getProducts = asynhandler(async(req, res) => {
 
     try {
         const product = await Product.find({})
         res.status(200).json(product)
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500)
+        throw new Error(error.message)
     }
-}
+})
 
-const getProduct = async(req, res) => {
+const getProduct = asynhandler(async(req, res) => {
 
     try {
         const { id } = req.params
@@ -28,36 +30,40 @@ const getProduct = async(req, res) => {
         throw new Error(error.message)
 
     }
-}
+})
 
-const deleteProduct = async(req, res) => {
+const deleteProduct = asynhandler(async(req, res) => {
     try {
         const { id } = req.params
         const product = await Product.findByIdAndDelete(id)
         if (!product) {
-            res.status(404, `Cannot find the product with id ${id}`)
+            res.status(404)
+            throw new Error(`Cannot find the product with id ${id}`)
+
         }
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
-}
+})
 
-const updateProduct = async(req, res) => {
+const updateProduct = asynhandler(async(req, res) => {
     try {
         const { id } = req.params
         const product = await Product.findByIdAndUpdate(id, req.body)
         if (!product) {
-            res.status(404, `Cannot find the product with id ${id}`)
+            res.status(404)
+            throw new Error(`Cannot find the product with id ${id}`)
+
         }
         const updatedProduct = await Product.findById(id)
         res.status(200).json(updatedProduct)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
-}
+})
 
-const createProduct = async(req, res) => {
+const createProduct = asynhandler(async(req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
@@ -65,8 +71,7 @@ const createProduct = async(req, res) => {
         console.log(error.message)
         res.status(500).json({ message: error.message })
     }
-}
-
+})
 
 
 module.exports = {
